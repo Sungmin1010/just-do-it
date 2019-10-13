bc43f0cdf6d06bc7d1d79fc9f90b52cb9e40d921
Sungmin-PC <lmsgsm1@gmail.com>
2019-10-14 01:53:12 +0900

    [new] day12

---
+++ algorithm/javaworkspace/30-days-of-code/src/Day12_Inheritance.java
@@ -0,0 +1,10 @@
+
+public class Day12_Inheritance {
+//Practice>Tutorials>30DaysofCode>Day12:Inheritance
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
