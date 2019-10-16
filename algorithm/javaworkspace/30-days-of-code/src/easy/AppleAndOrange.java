d8b35ce6f6dfec897f0179406712234bcb8d959d
Sungmin-PC <lmsgsm1@gmail.com>
2019-10-17 01:21:20 +0900

    [ing] 테스트코드 추가

--- algorithm/javaworkspace/30-days-of-code/src/easy/AppleAndOrange.java
+++ algorithm/javaworkspace/30-days-of-code/src/easy/AppleAndOrange.java
@@ -1,17 +1,36 @@
 package easy;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class AppleAndOrange {
 
-	static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+	static int[] countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
 
-
+		int[] output = {1,1};
+		
+		return output;
     }
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 	
+	@Test
+	public void testCountApplesAndOranges() throws Exception {
+		//given
+		int s = 7;
+		int t = 11;
+		int a = 5;
+		int b = 15;
+		int m = 3;
+		int n = 2;
+		int[] apples = {-2, 2, 1};
+		int[] orrange = {5, -6};
+		//when
+		int[] output = countApplesAndOranges(s, t, a, b, apples, orrange);
+		//then
+		assertEquals(1, output[0]);
+		assertEquals(1, output[1]);
+	}
 	
 
 }
