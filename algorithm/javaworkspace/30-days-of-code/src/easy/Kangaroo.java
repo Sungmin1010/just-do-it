080160cd0e3d557df5ee46bbfcdd94a69aa417c4
Sungmin-PC <lmsgsm1@gmail.com>
2019-10-22 00:11:28 +0900

    [new] Kangaroo

---
+++ algorithm/javaworkspace/30-days-of-code/src/easy/Kangaroo.java
@@ -0,0 +1,22 @@
+package easy;
+//https://www.hackerrank.com/challenges/kangaroo/problem
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Kangaroo {
+	
+	static String kangaroo(int x1, int v1, int x2, int v2) {
+
+
+    }
+
+	
+	@Test
+	public void testKangaroo() throws Exception {
+		//given
+		//when
+		//then
+		
+	}
+}
