d3b5973ff068be52eda5665b3ca95124271b634e
Sungmin-PC <lmsgsm1@gmail.com>
2019-10-22 00:27:02 +0900

    [ing] 8/30 test cases failed

--- algorithm/javaworkspace/30-days-of-code/src/easy/Kangaroo.java
+++ algorithm/javaworkspace/30-days-of-code/src/easy/Kangaroo.java
@@ -8,13 +8,22 @@ public class Kangaroo {
 	
 	static String kangaroo(int x1, int v1, int x2, int v2) {
 
+		int i=0;
+		
+		if(v1>v2) {
+			while(true) {
+				if( i*v1+x1>10000 || i*v2+x2>10000 ) break;
+				if( i*v1+x1 == i*v2+x2 ) return "YES";
+				i++;
+			}
+		}
 
-		return "";
+		return "NO";
     }
 
 	
 	@Test
-	public void testKangaroo() throws Exception {
+	public void testKangarooYes() throws Exception {
 		//given
 		int x1 = 0;
 		int v1 = 3;
@@ -23,7 +32,20 @@ public class Kangaroo {
 		//when
 		String result = kangaroo(x1, v1, x2, v2);
 		//then
-		assertEquals("yes", result);
+		assertEquals("YES", result);
 		
+	}
+	
+	@Test
+	public void testKangarooNo() throws Exception {
+		//given
+		int x1 = 0;
+		int v1 = 2;
+		int x2 = 5;
+		int v2 = 4;
+		//when
+		String result = kangaroo(x1, v1, x2, v2);
+		//then
+		assertEquals("NO", result);
 	}
 }
