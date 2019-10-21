55744f2362fe750b866dfc6adcf1a13b338d2485
Sungmin-PC <lmsgsm1@gmail.com>
2019-10-22 00:16:58 +0900

    [ing] 테스트 코드 추가

--- algorithm/javaworkspace/30-days-of-code/src/easy/Kangaroo.java
+++ algorithm/javaworkspace/30-days-of-code/src/easy/Kangaroo.java
@@ -9,14 +9,21 @@ public class Kangaroo {
 	static String kangaroo(int x1, int v1, int x2, int v2) {
 
 
+		return "";
     }
 
 	
 	@Test
 	public void testKangaroo() throws Exception {
 		//given
+		int x1 = 0;
+		int v1 = 3;
+		int x2 = 4;
+		int v2 = 2;
 		//when
+		String result = kangaroo(x1, v1, x2, v2);
 		//then
+		assertEquals("yes", result);
 		
 	}
 }
