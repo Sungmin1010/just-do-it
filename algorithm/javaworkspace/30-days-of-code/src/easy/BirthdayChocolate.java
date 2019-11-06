5438b33b05d202b5038345f9dbebd97681ef34de
Sungmin-PC <lmsgsm1@gmail.com>
2019-11-07 01:43:19 +0900

    Add test code to 'Birthday Chocolate'
    테스트케이스 하나만 작성.
    알고리즘 완성 후 파라미터라이즈로
    다수의 테스트 작성할 예정.

--- algorithm/javaworkspace/30-days-of-code/src/easy/BirthdayChocolate.java
+++ algorithm/javaworkspace/30-days-of-code/src/easy/BirthdayChocolate.java
@@ -1,6 +1,11 @@
 package easy;
 
+import static org.junit.Assert.*;
+
+
+import java.util.Arrays;
 import java.util.List;
+import org.junit.Test;
 
 //https://www.hackerrank.com/challenges/the-birthday-bar/problem
 public class BirthdayChocolate {
@@ -9,4 +14,16 @@ public class BirthdayChocolate {
 		
 		return 0;
     }
+	
+	@Test
+	public void testBirthday() throws Exception {
+		//given
+		List<Integer> s = Arrays.asList(1, 2, 1, 3, 2);
+		int d = 3;
+		int m = 2;
+		//when
+		int result = birthday(s, d, m);
+		//then
+		assertEquals(2, result);
+	}
 }
