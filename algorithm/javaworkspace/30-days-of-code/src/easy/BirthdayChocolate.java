4fe4c966afba564d40940f7d4992f1837aa635e5
Sungmin-PC <lmsgsm1@gmail.com>
2019-11-07 01:33:51 +0900

    Add 'Birthday Chocolate' problem

---
+++ algorithm/javaworkspace/30-days-of-code/src/easy/BirthdayChocolate.java
@@ -0,0 +1,12 @@
+package easy;
+
+import java.util.List;
+
+//https://www.hackerrank.com/challenges/the-birthday-bar/problem
+public class BirthdayChocolate {
+
+	static int birthday(List<Integer> s, int d, int m) {
+		
+		return 0;
+    }
+}
