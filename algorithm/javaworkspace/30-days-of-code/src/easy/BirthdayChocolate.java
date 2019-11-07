14272fbac65a7c44f2c93cbc7392bb4c1aad8b7e
Sungmin-PC <lmsgsm1@gmail.com>
2019-11-08 00:51:59 +0900

    Solve 'Birthday Chocolate'

--- algorithm/javaworkspace/30-days-of-code/src/easy/BirthdayChocolate.java
+++ algorithm/javaworkspace/30-days-of-code/src/easy/BirthdayChocolate.java
@@ -13,12 +13,12 @@ public class BirthdayChocolate {
 	//리스트로 푸는방법 : birthday2
 
 	static int birthday(List<Integer> s, int d, int m) {
-		//리스트 s를 배열로 만들어서 해결
+		//리스트 s를 배열로 변환 - List의 toArray 함수 사용
 		int result = 0;
 		Integer[] arrayS = s.toArray(new Integer[]{0});
 		//배열의 0부터 (길이-m)까지 포문을 돌면서
 		for(int i=0; i<=arrayS.length-m; i++) {
-			//i부터 i+m-1 까지 모두 더한 값이 d 가 되는지 확인
+			//i부터 (i+m-1) 까지 모두 더한 값이 d 가 되는지 확인
 			int sum = sumSubArray(arrayS, i, m);
 			if(sum==d) result++;
 		}
