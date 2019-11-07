50d53b2d4c44bb688ebb296418edde3a0fce01a7
Sungmin-PC <lmsgsm1@gmail.com>
2019-11-08 00:24:45 +0900

    Add code to 'Birthday Chocolate'
    List를 배열로 바꿔서 푸는 방법.
    테스트 코드 통과.

--- algorithm/javaworkspace/30-days-of-code/src/easy/BirthdayChocolate.java
+++ algorithm/javaworkspace/30-days-of-code/src/easy/BirthdayChocolate.java
@@ -9,11 +9,30 @@ import org.junit.Test;
 
 //https://www.hackerrank.com/challenges/the-birthday-bar/problem
 public class BirthdayChocolate {
+	
 
 	static int birthday(List<Integer> s, int d, int m) {
-		
-		return 0;
+		//리스트 s를 배열로 만들어서 해결
+		int result = 0;
+		Integer[] arrayS = s.toArray(new Integer[]{0});
+		//배열의 0부터 (길이-m)까지 포문을 돌면서
+		for(int i=0; i<=arrayS.length-m; i++) {
+			//i부터 i+m-1 까지 모두 더한 값이 d 가 되는지 확인
+			int sum = sumSubArray(arrayS, i, m);
+			if(sum==d) result++;
+		}
+		return result;
+		  
     }
+	
+	static int sumSubArray(Integer[] arr,int startIndx,int m) {
+		int totalSum = 0;
+		
+		for(int i=startIndx; i<startIndx+m; i++) {
+			totalSum += arr[i];
+		}
+		return totalSum;
+	}
 	
 	@Test
 	public void testBirthday() throws Exception {
