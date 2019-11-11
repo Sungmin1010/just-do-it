842a66d169a0fcc931e3bc7299ab7291dde20a4b
Sungmin-PC <lmsgsm1@gmail.com>
2019-11-11 22:52:12 +0900

    Add code for divisible sum paris

--- algorithm/javaworkspace/30-days-of-code/src/easy/DivisibleSumPairs.java
+++ algorithm/javaworkspace/30-days-of-code/src/easy/DivisibleSumPairs.java
@@ -8,7 +8,17 @@ import org.junit.Test;
 public class DivisibleSumPairs {
 	// i<j  and ar[i] + ar[j] % k == 0; 
 	static int divisibleSumPairs(int n, int k, int[] ar) {
-		return 0;
+	//i=0부터 i<n-1까지  for문을 돈다.
+	//ar[i]를 기준으로 i+1 인덱스 값부터 n-1 인덱스 까지 for문을 돈다 
+	//조건에 만족하는 값이 존재하는지 확인한다.
+		int result = 0;
+		
+		for(int i=0; i<n-1; i++) {
+			for(int j=i+1; j<n; j++) {
+				if( (ar[i]+ar[j]) % k == 0) result++;
+			}
+		}
+		return result;
 	}
 	
 	@Test
